05f3ccbf7620661bb74d20a77e3d7a4aee7fd986
zsabin <zsabin@gmail.com>
2017-04-20 13:36:09 -0400

    Removed pathExists condition used for testing

--- src/main.pl
+++ src/main.pl
@@ -30,28 +30,28 @@ translated([X,Y], [Dx,Dy], [X1,Y1]) :- X1 is X + Dx, Y1 is Y + Dy.
 % True if each point in the second list is equal to the corresponding point in the first
 % list, but scaled by Delta.
 translatedPoints([P], Delta, [P1]) :- translated(P, Delta, P1), point(P1).
-translatedPoints([P|T], Delta, [P1|T1]) :- 	translated(P, Delta, P1), 
-											translatedPoints(T, Delta, T1),
-											point(P1).
+translatedPoints([P|T], Delta, [P1|T1]) :-  translated(P, Delta, P1), 
+                                            translatedPoints(T, Delta, T1),
+                                            point(P1).
 
 % True if [X,Y] is equal to [X1,Y1] but scaled by [Sx, Sy]
-scaledVector([X,Y], [Sx,Sy], [X1,Y1]) :- X1 is X * Sx, Y1 is Y * Sy.											
+scaledVector([X,Y], [Sx,Sy], [X1,Y1]) :- X1 is X * Sx, Y1 is Y * Sy.                                            
 
 % True if [Dx,Dy] is a valid representation of a move vector
 delta([Dx,Dy]) :- Dx == 0 ; Dy == 0.
 
 % True if Result is a translation of Car by the specified delta relative to Car's direction
 translatedCar(Car, RelativeDelta, Result) :- direction(Car, Dir),
-											scaledVector([RelativeDelta, RelativeDelta], Dir, AbsoluteDelta), 
-											translatedPoints(Car, AbsoluteDelta, Result).
+                                            scaledVector([RelativeDelta, RelativeDelta], Dir, AbsoluteDelta), 
+                                            translatedPoints(Car, AbsoluteDelta, Result).
 
 % True if Result is a valid translation of the car at CarIndex of Car by the specified vector relative 
 % to the car's direction and constrained by the other cars on the grid
 validMove(Cars, [CarIndex, RelativeDelta], Result) :-  splitAtIndex(CarIndex, Cars, LeftCars, [Car|RightCars]),
-											translatedCar(Car, RelativeDelta, NewCar), 		
-											allSpacesAreUnoccupied(LeftCars, NewCar),
-											allSpacesAreUnoccupied(RightCars, NewCar),
-											append(LeftCars, [NewCar|RightCars], Result).
+                                            translatedCar(Car, RelativeDelta, NewCar),      
+                                            allSpacesAreUnoccupied(LeftCars, NewCar),
+                                            allSpacesAreUnoccupied(RightCars, NewCar),
+                                            append(LeftCars, [NewCar|RightCars], Result).
   
 % True if Left is a list containing the elements of List up to but not including Index
 % And Right is a list containing the rest of List.
@@ -59,14 +59,15 @@ splitAtIndex(N, List, [], List) :- N == 0.
 splitAtIndex(N, [H|T], [H|Left], Right) :- N1 is N - 1, splitAtIndex(N1, T, Left, Right).
 
 % True if Moves is a list of all the possible moves that could potenially be performed on the configuration of cars in Cars.
-% The moves returned in Moves are not necessarily valid moves, and must be checked for for the given configuration.
+% The moves returned in Moves are not necessarily valid moves, and must be checked for validity against the given configuration.
+% A Move is structured according to the specification detailed at the top of this file.
 hasMoves(Cars, Moves) :- length(Cars, Length), LastIndex is Length - 1, hasMoves(LastIndex, [], Moves).
 hasMoves(Index, PrevMoves, PrevMoves) :- Index == -1.
 hasMoves(Index, PrevMoves, Result) :- Index >= 0, NextIndex is Index - 1,
-									Move1 = [Index, 1], Move2 = [Index, -1],
-									NewMoves = [Move1,Move2],
-									append(NewMoves, PrevMoves, Moves),
-									hasMoves(NextIndex, Moves, Result).
+                                    Move1 = [Index, 1], Move2 = [Index, -1],
+                                    NewMoves = [Move1,Move2],
+                                    append(NewMoves, PrevMoves, Moves),
+                                    hasMoves(NextIndex, Moves, Result).
 
 % True if Point is not occupied by any of the specified cars
 isUnoccupiedSpace([], _).
@@ -77,14 +78,15 @@ allSpacesAreUnoccupied([], _).
 allSpacesAreUnoccupied(_, []).
 allSpacesAreUnoccupied(Cars, [Point|Points]) :- isUnoccupiedSpace(Cars, Point), allSpacesAreUnoccupied(Cars, Points).
 
+% Main function used to determine if a solution exists for a given board configuration
 % True if a solution can be reached from the configuration of cars represented in Config
 solutionExists(Config, Solution) :- solutionExists([], Config, [], Solution).
 
 % True if a solution can be reached from the configuration of cars represented in Config without transitioning to 
 % one of the configurations in PrevConfigs.
 solutionExists(_, Config, PrevMoves, Solution) :- pathExists(Config), reverse(PrevMoves, Solution).
-solutionExists(PrevConfigs, Config, PrevMoves, Solution) :- 	hasMoves(Config, Moves),
-																solutionExists(PrevConfigs, Config, PrevMoves, Moves, Solution).
+solutionExists(PrevConfigs, Config, PrevMoves, Solution) :-     hasMoves(Config, Moves),
+                                                                solutionExists(PrevConfigs, Config, PrevMoves, Moves, Solution).
 
 % True if a solution can be reached from the configuration of cars represented in Config by following one of the specified moves
 % without transitioning to one of the configurations in PrevConfigs, 
@@ -94,7 +96,7 @@ solutionExists(PrevConfigs, Config, PrevMoves, [_|Moves], Solution) :- solutionE
 % True if a solution can be reached from the configuration of cars represented in Config by following the specified move
 % without transitioning to one of the configurations in PrevConfigs, 
 solutionExistsWithMove(PrevConfigs, Config, PrevMoves, Move, Solution) :- validMove(Config, Move, NewConfig),
-																		\+ member(NewConfig, PrevConfigs),
-																		solutionExists([Config|PrevConfigs], NewConfig, [Move|PrevMoves], Solution).
+                                                                        \+ member(NewConfig, PrevConfigs),
+                                                                        solutionExists([Config|PrevConfigs], NewConfig, [Move|PrevMoves], Solution).
 
-pathExists(Cars) :- X = [[5,4], [4,4]], member(X, Cars).
+pathExists(_).
